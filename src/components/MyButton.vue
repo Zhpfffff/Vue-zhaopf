74c251661317828506acf6913080412e7a7134f4
Zhpfffff <1119447216@qq.com>
2021-08-27 17:38:48 +0800

    first

---
+++ src/components/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>我爱袁莉</h2>
+    <button @click="count++">爱了{{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style>
+h2 {
+  color: hotpink;
+}
+</style>
